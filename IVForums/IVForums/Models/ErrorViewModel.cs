1ebbaaa8349e5e25aa2af16c63f17bc678b63ef9
ivanbarraza <ivan.barraza@yahoo.com>
2019-02-11 16:50:10 -0700

    Initial Creation

---
+++ IVForums/IVForums/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IVForums.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
